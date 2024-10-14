634bdc80a31afd9068b90cccebfe87e1ecd78cfd
annasviri <anna.sviridova@databricks.com>
2024-10-14 13:05:19 +0200

    Update CICD test.py changes from repo side

--- Test_commitandmerge/CICD test.py
+++ Test_commitandmerge/CICD test.py
@@ -4,3 +4,4 @@
 # MAGIC ##After this it will be the test of merge##
 # MAGIC
 # MAGIC This is a test notebook to see how the commit and merge works between diff branches
+# MAGIC this is the row to see how the conflicts work if the changes are provided from repo side
